e5d3b96d6553027fd80349e1be86a0e6a42e661e
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2022-10-30 15:09:26 -0400

    modified script.js so a 5 day forecast is returned from the API instead of the first 5 3-hour blocks

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,7 @@
 const searchBtn = document.getElementById("searchBtn");
 
+var openWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}';
+
 const searchResults = document.getElementById("search-results");
 searchBtn.addEventListener("click", citySearch);
 
@@ -18,59 +20,64 @@ function citySearch(event) {
   };
 
 
-function searchCity(city, state, country, units) {
+function searchCity(city, units) {
     var myKey = '32f5f1296c8b541458176503242fec0c';
-    var cityState = state;
-    var cityCountry = country;
+    //var cityState = state;
+    //var cityCountry = country;
     var chosenUnits = "&units=" + units;
-    /*
+  
     if (!chosenUnits) {
         return;
     }
-    if (!cityState) {
-        return;
-    }
-    if (!cityCountry) {
-        return;
-    } */
+  
 
-    var openWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}';
-
-  openWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + myKey+ "&units=imperial";
+  openWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city +  '&appid=' + myKey+ "&units=imperial";
 
   fetch(openWeatherUrl)
     .then(function (response) {
       if (!response.ok) {
         throw response.json();
-      }
+      } else {
       console.log(response);
       return response.json();
-    })
-
+    }
+  })
     .then(function (data) {
       console.log(data);
        
-        for (let i = 0; i < 5; i++) { 
-            
+        for (let i = 0; i < data.list.length; i+=8) { 
+        let iconURL = 'http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon +'@2x.png'
         var block = document.createElement("div");
-        block.innerHTML = "<h3>City: " 
+        var info = document.createElement("div");
+        var icon = document.createElement("img");
+        icon.className = "icon";
+        block.className = "block";
+        info.className = "info"
+        block.style.display = 'flex';
+        info.innerHTML = "<h3>City: " 
                         + data.city.name 
-                        + "</h3><br><p>Date: " 
+                        + "</h3><p>Date: " 
                         + data.list[i].dt_txt
-                        + "</p><br><p>Weather: " +data.list[i].weather[0].main +"/"+ data.list[i].weather[0].description[0].toUpperCase() + data.list[i].weather[0].description.slice(1) + "</p><p>Temperature: " 
+                        + "</p><p>Weather: " 
+                        + data.list[i].weather[0].main + "/"
+                        + data.list[i].weather[0].description[0].toUpperCase() 
+                        + data.list[i].weather[0].description.slice(1) 
+                        + "</p><p>Temperature: " 
                         + data.list[i].main.temp + " degrees "
-                        + "</p><br><p>Humidity: " 
+                        + "</p><p>Humidity: " 
                         + data.list[i].main.humidity +"%"
-                        + "</p><br><p>Wind Speed: "
+                        + "</p><p>Wind Speed: "
                         + data.list[i].wind.speed + " mph";
             console.log(data.list[i])
+        icon.setAttribute("src", iconURL);
         searchResults.appendChild(block);
-      
+        block.appendChild(info);
+        block.appendChild(icon);
     }
 })
     .catch(function (error) {
       console.error(error);
-    });
+    }); 
 };
 
 
